5b4eede715abc85a9b70c73c419ffbe36d41e92b
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2024-12-21 21:06:56 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-game:GetService('Players'):Kick('Changed Github path, please go back to Our Discord Server to get the new Script RESP PATH!\nInvite: https://discord.gg/MbQk76T2Ch')
+game:GetService('Players').LocalPlayer:Kick('Changed Github path, please go back to Our Discord Server to get the new Script RESP PATH!\nInvite: https://discord.gg/MbQk76T2Ch')
